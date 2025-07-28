24c19922fac36a908e802e30f9e5d4f1fddadfa8
zzzyans <limzhiyan05@gmail.com>
2025-07-28 13:08:26 +0800

    small bug in recommendation model + unit test update

--- services/RecommendationCacheService.js
+++ services/RecommendationCacheService.js
@@ -1,8 +1,8 @@
 // services/RecommendationCacheService.js
 import axios from 'axios';
 
-// const API_BASE_URL = "http://127.0.0.1:8000";
-const API_BASE_URL = "https://12102b64ef42.ngrok-free.app";
+const API_BASE_URL = "http://127.0.0.1:8000";
+// const API_BASE_URL = "https://3ed72e7f00c5.ngrok-free.app";
 
 class RecommendationCacheService {
   constructor() {
